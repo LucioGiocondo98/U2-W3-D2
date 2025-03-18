bca9d4281cfa01fc4b5ac4c4673f41b68626515d
LucioGiocondo98 <togaluc@hotmail.com>
2025-03-18 14:32:33 +0100

    end

---
+++ form.js
@@ -0,0 +1,33 @@
+const updateSavedValue = function() {
+    const savedName = localStorage.getItem('name');
+    const savedValueElement = document.getElementById('savedValue');
+    if (savedName) {
+        savedValueElement.textContent = 'Nome salvato: ' + savedName;
+    } else {
+        savedValueElement.textContent = ''; 
+    }
+}
+
+updateSavedValue();
+
+//Salva
+const savedButton = document.getElementById('saveButton');
+savedButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    const nameInput = document.getElementById('name').value;
+    if (nameInput) {
+        localStorage.setItem('name', nameInput);
+        updateSavedValue(); 
+    } else {
+        alert('Please, insert name');
+    }
+});
+
+//Rimuovi
+const removeButton = document.getElementById('removeButton');
+removeButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    localStorage.removeItem('name'); 
+    document.getElementById('name').value = ''; 
+    updateSavedValue(); 
+});
